abd54827d0212358a045af0356f3e2cc0d4aa7f6
Phoenix Gabriel <phoenix@openenergysolutionsinc.com>
2022-07-06 06:30:25 -0700

    Remove Users component * Users no longer editable form this hmi

--- Client/src/app/settings/settings-routing.module.ts
+++ Client/src/app/settings/settings-routing.module.ts
@@ -5,7 +5,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppSettingsComponent } from './app-settings/app-settings.component';
-import { UsersComponent } from './users/users.component';
 import { TagsComponent } from './tags/tags.component';
 import { DevicesComponent } from './devices/devices.component';
 
@@ -17,10 +16,6 @@ const routes: Routes = [
         path: 'general',
         component: AppSettingsComponent,
         data: { title: 'GENERAL' }
-      }, {
-        path: 'users',
-        component: UsersComponent,
-        data: { title: 'USERS' }
       }, {
         path: 'tags',
         component: TagsComponent,
--- Client/src/app/settings/settings.module.ts
+++ Client/src/app/settings/settings.module.ts
@@ -7,26 +7,24 @@ import { CommonModule } from '@angular/common';
 
 import { SettingsRoutingModule } from './settings-routing.module';
 import { AppSettingsComponent } from './app-settings/app-settings.component';
-import { UsersComponent } from './users/users.component';
 import { TagsComponent } from './tags/tags.component';
 import { SharedComponentsModule } from '../shared/components/shared-components.module';
 import { SharedMaterialModule } from '../shared/shared-material.module';
 import { SharedModule } from '../shared/shared.module';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { DialogsComponent } from './users/dialogs/dialogs.component';
 import { DevicesComponent } from './devices/devices.component';
 import { DeviceDialogsComponent } from './devices/dialogs/devicedialogs.component';
 
 
 @NgModule({
-  declarations: [AppSettingsComponent, UsersComponent, TagsComponent, DialogsComponent, DevicesComponent, DeviceDialogsComponent],
+  declarations: [AppSettingsComponent, TagsComponent, DevicesComponent, DeviceDialogsComponent],
   imports: [
     CommonModule,
     SettingsRoutingModule,
     SharedComponentsModule,
     SharedMaterialModule,
     SharedModule,
-    NgxDatatableModule 
+    NgxDatatableModule
   ]
 })
 export class SettingsModule { }
