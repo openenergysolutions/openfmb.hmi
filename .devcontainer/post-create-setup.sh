8cf7c62824c08ed39007a9c3d98d906548375c59
Seth Gossler <seth@openenergysolutionsinc.com>
2022-10-19 05:43:36 +0000

    Update - added a lot of work to make full stack dev easier. Added -crt-static to the Rustflags in the Dockerfile. Alpine musl libs are dynamic, so libs like libressl couldnt resolve.
    Signed-off-by: Seth Gossler <seth@openenergysolutionsinc.com>

---
+++ .devcontainer/post-create-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Git submodule upset and init
+git submodule update --init --recursive
+
+# Install mkcert
+curl -s https://api.github.com/repos/FiloSottile/mkcert/releases/latest| grep browser_download_url  | grep linux-amd64 | cut -d '"' -f 4 | wget -qi -
+mv mkcert-v*-linux-amd64 mkcert
+chmod a+x mkcert
+sudo mv mkcert /usr/local/bin/
+
+# Init Certs
+export LOCAL_IP=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+mkdir certs
+cd certs
+mkdir server
+cd server
+mkcert -install
+mkcert -cert-file server-cert.pem -key-file server-key.pem localhost 127.0.0.1 $LOCAL_IP ::1
+mkcert -CAROOT
+cd ..
+cp -r ~/.local/share/mkcert/. .
+cd ..
+
+# Apt Packages
+# sudo apt-get install autoconf automake libtool curl make g++ unzip cmake
+
+# Project Yarn Dependencies
+npm install -g @angular/cli
+npm install --prod=false
+cd Client
+yarn install
+yarn run build
+cd ..
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+
+echo "post-create-setup.sh done!"
