f719f4b2da38715c774d8969fd0aed882152de55
Phoenix Gabriel <phoenix@openenergysolutionsinc.com>
2023-02-16 21:35:44 +0000

    Fix Keycloak compatibility - WIP
    Signed-off-by: Phoenix Gabriel <phoenix@openenergysolutionsinc.com>
    Signed-off-by: Seth Gossler <seth@openenergysolutionsinc.com>

--- Client/src/environments/environment.prod.ts
+++ Client/src/environments/environment.prod.ts
@@ -5,11 +5,12 @@
 import envSettings from '../assets/env_prod.json';
 
 export const environment = {
-  production: false,
+  production: true,
   apiUrl: envSettings.apiUrl,
   ws: envSettings.ws,
   auth: {
     authorize_path: envSettings.auth_authorize_path,
+    token_path: envSettings.auth_token_path,
     audience: envSettings.auth_audience,
     client_id: envSettings.auth_client_id,
     domain: envSettings.auth_domain,
